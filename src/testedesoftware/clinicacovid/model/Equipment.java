ThiagoACampos/tp1-teste-de-software

commit 0fc55e8cd35271901a87c01e769b3cb1ecc8b02b

[class Equipment{}; equipmentTest{}] Adicionando Testes de Unidade

src/testedesoftware/clinicacovid/model/Equipment.java
@@ -1,18 +1,24 @@
 package testedesoftware.clinicacovid.model;
 
 public class Equipment {
-	
+
 	private String name;
 	private int quantity;
 	private boolean consumable;
-	
+	private int max;
+
+	public String exceptionEquipmentNoLongerAvailableOrCannotBeUsed = "Equipamento sem unidades disponíveis ou não consumível";
+	public String exceptionEquipmentQuantityMustBePositive = "Quantidade de um equipamento deve ser positiva.";
+	public String exceptionEquipmentQuantityExceedsMaximum = "A capacidade máxima de armazenamento é " + this.max + ".";
+
 	public Equipment(String name, int quantity, boolean consumable) {
 		super();
 		this.name = name;
 		this.quantity = quantity;
 		this.consumable = consumable;
+		this.max = 100;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
@@ -20,26 +26,47 @@ public class Equipment {
 	public int getQuantity() {
 		return quantity;
 	}
-	
+
+	public int getMax() {
+		return max;
+	}
+
 	public void setQuantity(int quantity) {
+		this.checkQuantity(quantity);
 		this.quantity = quantity;
 	}
 
+	public void setStatus(boolean equipmentStatus) {
+		this.consumable = equipmentStatus;
+	}
+
 	public void use() throws Exception {
-		if(consumable && quantity > 0) {
+		if(this.canUse()) {
 			quantity--;
 		}
 		else{
-			throw new Exception("Equipamento sem unidades disponíveis ou não consumível");
+			throw new Exception(this.exceptionEquipmentNoLongerAvailableOrCannotBeUsed);
 		}
 	}
-	
+
 	public boolean canUse() {
 		return quantity > 0 && consumable;
 	}
-	
-	public void addUnits(int qtt) {
+
+	public void addUnits(int qtt) throws Exception {
+		this.checkQuantity(qtt);
+		if (this.quantity + qtt > this.max) {
+			throw new Exception(this.exceptionEquipmentQuantityExceedsMaximum);
+		}
 		quantity += qtt;
 	}
 
+	private void checkQuantity(int quantity) throws Exception {
+		if (quantity < 0) {
+			throw new Exception(this.exceptionEquipmentQuantityMustBePositive);
+		}
+		if (quantity > this.max) {
+			throw new Exception(this.exceptionEquipmentQuantityExceedsMaximum);
+		}
+	}
 }

src/test/testedesoftware/clinicacovid/unitTests/EquipmentTest.java
@@ -1,50 +1,168 @@
 package test.testedesoftware.clinicacovid.unitTests;
 
-import org.junit.jupiter.api.Test;
-
 import testedesoftware.clinicacovid.model.Equipment;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class EquipmentTest {
-  
-  Equipment equipment = new Equipment("Seringa", 10, true);
+  private Equipment equipment;
+
+  @BeforeEach
+  void setUp() {
+    equipment = new Equipment("Seringa", 10, true);
+  }
 
   @Test
-  void testUseEquipmentSuccess() throws Exception {
+  void testCanUse() {
+    assertTrue(equipment.canUse());
+  }
+
+  @Test
+  void testCannotUse() {
+    equipment.setStatus(false);
+    assertFalse(equipment.canUse());
+  }
+
+  @Test
+  void testUseNonConsumableEquipment() throws Exception {
+    equipment.setStatus(false);
+    Exception exception = assertThrows(Exception.class, () -> {
+      equipment.use();
+    });
+    assertEquals(equipment.exceptionEquipmentNoLongerAvailableOrCannotBeUsed, exception.getMessage());
+  }
+
+  @Test
+  void testUseConsumableEquipmentSuccess() {
     equipment.use();
     assertEquals(9, equipment.getQuantity());
   }
 
   @Test
-  void testUseEquipmentFailure() throws Exception {
+  void testUseConsumableEquipmentFailure() throws Exception {
     final Exception exception = assertThrows(Exception.class, () -> {
       for(int i = 0; i < 20; i++){
         equipment.use();
       }
     });
 
-    assertEquals("Equipamento sem unidades disponíveis ou não consumível", exception.getMessage());
+    assertEquals(
+      equipment.exceptionEquipmentNoLongerAvailableOrCannotBeUsed,
+      exception.getMessage()
+    );
   }
 
   @Test
   void testAddUnits() {
     equipment.addUnits(21);
-
     assertEquals(31, equipment.getQuantity());
   }
 
   @Test
-  void testCanUse() {
-    if (equipment.getQuantity() > 0)
-      assertTrue(equipment.canUse());
-    else 
-      assertFalse(equipment.canUse());
+  void testAddAndUseUnitsUntilSpent() {
+    equipment.addUnits(21);
+    assertEquals(31, equipment.getQuantity());
+    for(int i = 0; i < 31; i++){
+      equipment.use();
+    }
+    assertEquals(0, equipment.getQuantity());
   }
 
   @Test
-  void testNameEquipment() {
-    assertEquals("Seringa", equipment.getName());
+  void testUseAtMaxQuantity() throws Exception {
+    // Test if setting quantity to max.
+    equipment.setQuantity(equipment.getMax());
+    for(int i = 0; i < equipment.getMax(); i++) {
+      equipment.use();
+    }
+    assertEquals(0, equipment.getQuantity());
+    // Makes the equipment unusable after max uses.
+    assertFalse(equipment.canUse());
   }
 
+  /*
+   * The only way to subtract equipment quantity should be
+   * through the .use() function
+   */
+  @Test
+  void testAddNegativeQuantity() throws Exception {
+    // A user should not be able to add a negative quantity.
+    final Exception exceptionAddNegativeQuantity = assertThrows(Exception.class, () -> {
+      equipment.addUnits(-1);
+    });
+    assertEquals(
+      equipment.exceptionEquipmentQuantityMustBePositive,
+      exceptionAddNegativeQuantity.getMessage()
+    );
+    // And the quantity must remain the same after the operation fails.
+    assertEquals(10, equipment.getQuantity());
+  }
+
+  @Test
+  void testSetNegativeQuantity() throws Exception {
+    // A user should not be able to set a negative quantity.
+    final Exception exceptionSetNegativeQuantity = assertThrows(Exception.class, () -> {
+      equipment.setQuantity(-1);
+    });
+    assertEquals(
+      equipment.exceptionEquipmentQuantityMustBePositive,
+      exceptionSetNegativeQuantity.getMessage()
+    );
+    // And the quantity must remain the same after the operation fails.
+    assertEquals(10, equipment.getQuantity());
+  }
+
+  @Test
+  void testCheckQuantityAtMaximum() {
+    // The maximum number of units for a given equipment is 100.
+    equipment.setQuantity(100);
+    assertEquals(100, equipment.getQuantity());
+
+    equipment.setQuantity(1);
+    equipment.addUnits(99);
+    assertEquals(100, equipment.getQuantity());
+  }
+
+  @Test
+  void testAddQuantityAboveMaximum() throws Exception {
+    // If a user tries to add a quantity that exceeds the maximum of 100.
+    final Exception exceptionAddUnitsAboveMaximum = assertThrows(Exception.class, () -> {
+      equipment.addUnits(91);
+    });
+    // An exception must be thrown.
+    assertEquals(
+      equipment.exceptionEquipmentQuantityExceedsMaximum,
+      exceptionAddUnitsAboveMaximum.getMessage()
+    );
+    // And the equipment quantity must remain the same.
+    assertEquals(10, equipment.getQuantity());
+  }
+
+  @Test
+  void testSetQuantityAboveMaximum() throws Exception {
+    // If a user tries to Set a quantity that exceeds the maximum of 100.
+    final Exception exceptionSetUnitsAboveMaximum = assertThrows(Exception.class, () -> {
+      equipment.setQuantity(101);
+    });
+    // An exception must be thrown.
+    assertEquals(
+      equipment.exceptionEquipmentQuantityExceedsMaximum,
+      exceptionSetUnitsAboveMaximum.getMessage()
+    );
+    // And the equipment quantity must remain the same.
+    assertEquals(10, equipment.getQuantity());
+  }
+
+  @Test
+  void testSetQuantityAndAddQuantityThatExceedsMaximum() throws Exception {
+    equipment.setQuantity(50);
+    final Exception exception = assertThrows(Exception.class, () -> {
+      equipment.addUnits(51);
+    });
+    assertEquals(equipment.exceptionEquipmentQuantityExceedsMaximum, exception.getMessage());
+    // Quantity should remain unchanged.
+    assertEquals(50, equipment.getQuantity());
+  }
 }

commit fbfc3b79ef91af5b58454cf11f075a36291db771

[class: Storage{}] Fix side effect caused by changes in commit 0fc55e8

src/testedesoftware/clinicacovid/model/Equipment.java
@@ -31,7 +31,7 @@ public class Equipment {
 		return max;
 	}
 
-	public void setQuantity(int quantity) {
+	public void setQuantity(int quantity) throws Exception {
 		this.checkQuantity(quantity);
 		this.quantity = quantity;
 	}

src/testedesoftware/clinicacovid/model/Storage.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public class Storage {
 	private List<Equipment> equipments;
-	
+
 	public Storage() {
 		equipments = new ArrayList<>();
 	}
@@ -27,35 +27,35 @@ public class Storage {
 		}
 		return missing;
 	}
-	
+
 	public void useEquipments() throws Exception {
 		for(Equipment eq : equipments) {
 			eq.use();
 		}
 	}
-	
-	public void addUnits(int eqIndex, int units) {
+
+	public void addUnits(int eqIndex, int units) throws Exception {
 		equipments.get(eqIndex).addUnits(units);
 	}
-	
+
 	public void addEquipment(Equipment eq) {
 		equipments.add(eq);
 	}
-	
+
 	public File saveEquipmentsToFile(File fileToSave) throws IOException {
-		
+
         FileWriter fstream = new FileWriter(fileToSave.getAbsolutePath(), StandardCharsets.ISO_8859_1,true);
-        
+
         for(Integer i = 0; i < this.equipments.size(); i++) {
         	Equipment current_equipment = this.getEquipments().get(i);
 	        fstream.write(current_equipment.getName());
 	        fstream.write(current_equipment.getQuantity());
 	        fstream.write(Boolean.toString(current_equipment.canUse()));
         }
-        
+
         fstream.close();
-        
+
         return fileToSave;
-        
+
 	}
 }

src/test/testedesoftware/clinicacovid/integrationTests/IntegrationTests.java
@@ -21,17 +21,17 @@ import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class IntegrationTests {
-	
+
 	private Doctor doctor;
 	private Patient patient;
 	private Storage storage;
-	
+
 	@Rule
 	public TemporaryFolder folder = new TemporaryFolder();
-	
+
 	@BeforeEach
 	protected void dataSetup() throws Exception {
-		
+
 	    patient = new Patient("José",  20, "jose@gmail.com", "31999999999", "jose");
 	    doctor = new Doctor("AB1234", "Roberto", "roberto");
 	    storage = new Storage();
@@ -43,26 +43,26 @@ public class IntegrationTests {
 
     public IntegrationTests() throws ParseException {
     }
-    
+
     @Test
     void testPacientScheduleAndCancelAppointment() throws Exception {
-    	
+
     	// Scheduling Appointment
     	Date scheduleDate = new SimpleDateFormat("yyyy-MM-dd").parse("2022-07-08");
     	DoctorAppointment docAppointment = new DoctorAppointment(doctor, scheduleDate, patient);
     	doctor.getCalendar().getAppointments().add(docAppointment);
-    	
+
     	// Canceling Appointment
     	Calendar doctorCalendar = doctor.getCalendar();
     	doctorCalendar.cancel(docAppointment);
-    	
+
     	List<Appointment> doctorAppointments = doctor.getCalendar().getAppointments();
     	assertTrue(doctorAppointments.contains(docAppointment) == false);
     }
-    
+
     @Test
     void testCancelAppointmentAndCheckingFilterDay() throws Exception {
-    	
+
     	// Scheduling Appointment
     	Date scheduleDateOne = new SimpleDateFormat("yyyy-MM-dd").parse("2022-07-08");
     	Date scheduleDateTwo = new SimpleDateFormat("yyyy-MM-dd").parse("2022-07-15");
@@ -70,75 +70,75 @@ public class IntegrationTests {
     	DoctorAppointment docAppointmentTwo = new DoctorAppointment(doctor, scheduleDateTwo, patient);
     	doctor.getCalendar().getAppointments().add(docAppointmentOne);
     	doctor.getCalendar().getAppointments().add(docAppointmentTwo);
-    	
+
     	// Canceling Appointment
     	Calendar doctorCalendar = doctor.getCalendar();
     	doctorCalendar.cancel(docAppointmentOne);
-    	
+
     	assertTrue(doctorCalendar.filterDay(scheduleDateOne).size() == 0);
     	assertTrue(doctorCalendar.filterDay(scheduleDateTwo).size() == 1);
     }
-    
+
     @Test
-    void testCovidTestWithoutEquipmentExceptionThrown() throws ParseException {
-    	
+    void testCovidTestWithoutEquipmentExceptionThrown() throws ParseException, Exception {
+
     	// Scheduling Covid Test Appointment
     	Nurse nurse = new Nurse("Luana","luana");
     	Date scheduleDate = new SimpleDateFormat("yyyy-MM-dd").parse("2022-01-15");
     	CovidTestAppointment covidTestAppointment = new CovidTestAppointment(nurse,scheduleDate,patient);
     	doctor.getCalendar().getAppointments().add(covidTestAppointment);
-    	
+
     	// Setting Equipment quantity to zero
     	List<Equipment> storageEquipments = storage.getEquipments();
     	Equipment covidEquipment = storageEquipments.stream()
     		.filter(equipment -> "Teste Covid".equals(equipment.getName()))
     		.findAny()
     		.orElse(null);
-    	
+
     	if(covidEquipment != null)
     		covidEquipment.setQuantity(0);
-    	
+
     	// Using not available equipment
     	Exception exception = assertThrows(Exception.class, () -> {
     		covidEquipment.use();
         });
-    	
+
     	String expectedMessage = "Equipamento sem unidades disponíveis ou não consumível";
     	String actualMessage = exception.getMessage();
-    	
+
     	assertTrue(actualMessage.equals(expectedMessage));
     }
 
     @Test
     void testScheduleAppointmentAndSaveToFile() throws ParseException, IOException {
-    	
+
     	// Scheduling Appointment
     	Date date = new SimpleDateFormat("yyyy-MM-dd").parse("2022-10-12");
         DoctorAppointment docAppointment = new DoctorAppointment(doctor, date, patient);
         doctor.getCalendar().getAppointments().add(docAppointment);
         Appointment newAppointment = doctor.getCalendar().getAppointments().get(0);
-        
+
         //Saving to file
         File fileToSave = folder.newFile("schedule_appointment.txt");
         newAppointment.saveToFile(fileToSave);
-        
+
         final String fileContent = Files.readString(fileToSave.toPath(), StandardCharsets.ISO_8859_1);
-        
+
         assertEquals(1, doctor.getCalendar().getAppointments().size());
         assertTrue(fileToSave.exists());
         assertTrue(fileContent.contains(newAppointment.getPatient().getName()));
         assertTrue(fileContent.contains(Integer.toString(newAppointment.getPatient().getAge())));
         assertTrue(fileContent.contains(newAppointment.getPatient().getEmail()));
     }
-    
+
     @Test
     void testStorageSaveEquipmentsToFile() throws IOException {
-    	
+
     	//Saving to file
         File fileToSave = folder.newFile("storage_equipments.txt");
-        
+
         storage.saveEquipmentsToFile(fileToSave);
-        
+
         final String fileContent = Files.readString(fileToSave.toPath(), StandardCharsets.ISO_8859_1);
         assertTrue(fileToSave.exists());
         assertTrue(fileContent.contains("Teste Covid"));

src/test/testedesoftware/clinicacovid/unitTests/EquipmentTest.java
@@ -35,7 +35,7 @@ public class EquipmentTest {
   }
 
   @Test
-  void testUseConsumableEquipmentSuccess() {
+  void testUseConsumableEquipmentSuccess() throws Exception {
     equipment.use();
     assertEquals(9, equipment.getQuantity());
   }
@@ -55,13 +55,13 @@ public class EquipmentTest {
   }
 
   @Test
-  void testAddUnits() {
+  void testAddUnits() throws Exception {
     equipment.addUnits(21);
     assertEquals(31, equipment.getQuantity());
   }
 
   @Test
-  void testAddAndUseUnitsUntilSpent() {
+  void testAddAndUseUnitsUntilSpent() throws Exception {
     equipment.addUnits(21);
     assertEquals(31, equipment.getQuantity());
     for(int i = 0; i < 31; i++){
@@ -115,7 +115,7 @@ public class EquipmentTest {
   }
 
   @Test
-  void testCheckQuantityAtMaximum() {
+  void testCheckQuantityAtMaximum() throws Exception  {
     // The maximum number of units for a given equipment is 100.
     equipment.setQuantity(100);
     assertEquals(100, equipment.getQuantity());

src/test/testedesoftware/clinicacovid/unitTests/StorageTest.java
@@ -9,9 +9,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StorageTest {
-  
-  Equipment seringa = new Equipment("Seringa", 250, false);
-  Equipment pcr = new Equipment("PCR", 300, true);
+
+  Equipment seringa = new Equipment("Seringa", 25, false);
+  Equipment pcr = new Equipment("PCR", 30, true);
 
   Storage storage = new Storage();
 
@@ -29,15 +29,15 @@ public class StorageTest {
     storage.useEquipments();
 
     assertEquals(1, storage.getEquipments().size());
-    assertEquals(299, pcr.getQuantity());
+    assertEquals(29, pcr.getQuantity());
   }
 
   @Test
-  void testAddUnits() {
+  void testAddUnits() throws Exception {
     storage.addEquipment(pcr);
     storage.addUnits(0, 10);
 
-    assertEquals(310, pcr.getQuantity());
+    assertEquals(40, pcr.getQuantity());
   }
 
   @Test