0fc55e8cd35271901a87c01e769b3cb1ecc8b02b
Leandro Souza <48248990+LeandroMvSouza@users.noreply.github.com>
2023-05-20 11:59:45 -0300

    [class Equipment{}; equipmentTest{}] Adicionando Testes de Unidade

--- src/test/testedesoftware/clinicacovid/unitTests/EquipmentTest.java
+++ src/test/testedesoftware/clinicacovid/unitTests/EquipmentTest.java
@@ -1,50 +1,168 @@
 package test.testedesoftware.clinicacovid.unitTests;
 
-import org.junit.jupiter.api.Test;
-
 import testedesoftware.clinicacovid.model.Equipment;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class EquipmentTest {
-  
-  Equipment equipment = new Equipment("Seringa", 10, true);
+  private Equipment equipment;
+
+  @BeforeEach
+  void setUp() {
+    equipment = new Equipment("Seringa", 10, true);
+  }
 
   @Test
-  void testUseEquipmentSuccess() throws Exception {
+  void testCanUse() {
+    assertTrue(equipment.canUse());
+  }
+
+  @Test
+  void testCannotUse() {
+    equipment.setStatus(false);
+    assertFalse(equipment.canUse());
+  }
+
+  @Test
+  void testUseNonConsumableEquipment() throws Exception {
+    equipment.setStatus(false);
+    Exception exception = assertThrows(Exception.class, () -> {
+      equipment.use();
+    });
+    assertEquals(equipment.exceptionEquipmentNoLongerAvailableOrCannotBeUsed, exception.getMessage());
+  }
+
+  @Test
+  void testUseConsumableEquipmentSuccess() {
     equipment.use();
     assertEquals(9, equipment.getQuantity());
   }
 
   @Test
-  void testUseEquipmentFailure() throws Exception {
+  void testUseConsumableEquipmentFailure() throws Exception {
     final Exception exception = assertThrows(Exception.class, () -> {
       for(int i = 0; i < 20; i++){
         equipment.use();
       }
     });
 
-    assertEquals("Equipamento sem unidades disponíveis ou não consumível", exception.getMessage());
+    assertEquals(
+      equipment.exceptionEquipmentNoLongerAvailableOrCannotBeUsed,
+      exception.getMessage()
+    );
   }
 
   @Test
   void testAddUnits() {
     equipment.addUnits(21);
-
     assertEquals(31, equipment.getQuantity());
   }
 
   @Test
-  void testCanUse() {
-    if (equipment.getQuantity() > 0)
-      assertTrue(equipment.canUse());
-    else 
-      assertFalse(equipment.canUse());
+  void testAddAndUseUnitsUntilSpent() {
+    equipment.addUnits(21);
+    assertEquals(31, equipment.getQuantity());
+    for(int i = 0; i < 31; i++){
+      equipment.use();
+    }
+    assertEquals(0, equipment.getQuantity());
   }
 
   @Test
-  void testNameEquipment() {
-    assertEquals("Seringa", equipment.getName());
+  void testUseAtMaxQuantity() throws Exception {
+    // Test if setting quantity to max.
+    equipment.setQuantity(equipment.getMax());
+    for(int i = 0; i < equipment.getMax(); i++) {
+      equipment.use();
+    }
+    assertEquals(0, equipment.getQuantity());
+    // Makes the equipment unusable after max uses.
+    assertFalse(equipment.canUse());
   }
 
+  /*
+   * The only way to subtract equipment quantity should be
+   * through the .use() function
+   */
+  @Test
+  void testAddNegativeQuantity() throws Exception {
+    // A user should not be able to add a negative quantity.
+    final Exception exceptionAddNegativeQuantity = assertThrows(Exception.class, () -> {
+      equipment.addUnits(-1);
+    });
+    assertEquals(
+      equipment.exceptionEquipmentQuantityMustBePositive,
+      exceptionAddNegativeQuantity.getMessage()
+    );
+    // And the quantity must remain the same after the operation fails.
+    assertEquals(10, equipment.getQuantity());
+  }
+
+  @Test
+  void testSetNegativeQuantity() throws Exception {
+    // A user should not be able to set a negative quantity.
+    final Exception exceptionSetNegativeQuantity = assertThrows(Exception.class, () -> {
+      equipment.setQuantity(-1);
+    });
+    assertEquals(
+      equipment.exceptionEquipmentQuantityMustBePositive,
+      exceptionSetNegativeQuantity.getMessage()
+    );
+    // And the quantity must remain the same after the operation fails.
+    assertEquals(10, equipment.getQuantity());
+  }
+
+  @Test
+  void testCheckQuantityAtMaximum() {
+    // The maximum number of units for a given equipment is 100.
+    equipment.setQuantity(100);
+    assertEquals(100, equipment.getQuantity());
+
+    equipment.setQuantity(1);
+    equipment.addUnits(99);
+    assertEquals(100, equipment.getQuantity());
+  }
+
+  @Test
+  void testAddQuantityAboveMaximum() throws Exception {
+    // If a user tries to add a quantity that exceeds the maximum of 100.
+    final Exception exceptionAddUnitsAboveMaximum = assertThrows(Exception.class, () -> {
+      equipment.addUnits(91);
+    });
+    // An exception must be thrown.
+    assertEquals(
+      equipment.exceptionEquipmentQuantityExceedsMaximum,
+      exceptionAddUnitsAboveMaximum.getMessage()
+    );
+    // And the equipment quantity must remain the same.
+    assertEquals(10, equipment.getQuantity());
+  }
+
+  @Test
+  void testSetQuantityAboveMaximum() throws Exception {
+    // If a user tries to Set a quantity that exceeds the maximum of 100.
+    final Exception exceptionSetUnitsAboveMaximum = assertThrows(Exception.class, () -> {
+      equipment.setQuantity(101);
+    });
+    // An exception must be thrown.
+    assertEquals(
+      equipment.exceptionEquipmentQuantityExceedsMaximum,
+      exceptionSetUnitsAboveMaximum.getMessage()
+    );
+    // And the equipment quantity must remain the same.
+    assertEquals(10, equipment.getQuantity());
+  }
+
+  @Test
+  void testSetQuantityAndAddQuantityThatExceedsMaximum() throws Exception {
+    equipment.setQuantity(50);
+    final Exception exception = assertThrows(Exception.class, () -> {
+      equipment.addUnits(51);
+    });
+    assertEquals(equipment.exceptionEquipmentQuantityExceedsMaximum, exception.getMessage());
+    // Quantity should remain unchanged.
+    assertEquals(50, equipment.getQuantity());
+  }
 }
--- src/testedesoftware/clinicacovid/model/Equipment.java
+++ src/testedesoftware/clinicacovid/model/Equipment.java
@@ -1,18 +1,24 @@
 package testedesoftware.clinicacovid.model;
 
 public class Equipment {
-	
+
 	private String name;
 	private int quantity;
 	private boolean consumable;
-	
+	private int max;
+
+	public String exceptionEquipmentNoLongerAvailableOrCannotBeUsed = "Equipamento sem unidades disponíveis ou não consumível";
+	public String exceptionEquipmentQuantityMustBePositive = "Quantidade de um equipamento deve ser positiva.";
+	public String exceptionEquipmentQuantityExceedsMaximum = "A capacidade máxima de armazenamento é " + this.max + ".";
+
 	public Equipment(String name, int quantity, boolean consumable) {
 		super();
 		this.name = name;
 		this.quantity = quantity;
 		this.consumable = consumable;
+		this.max = 100;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
@@ -20,26 +26,47 @@ public class Equipment {
 	public int getQuantity() {
 		return quantity;
 	}
-	
+
+	public int getMax() {
+		return max;
+	}
+
 	public void setQuantity(int quantity) {
+		this.checkQuantity(quantity);
 		this.quantity = quantity;
 	}
 
+	public void setStatus(boolean equipmentStatus) {
+		this.consumable = equipmentStatus;
+	}
+
 	public void use() throws Exception {
-		if(consumable && quantity > 0) {
+		if(this.canUse()) {
 			quantity--;
 		}
 		else{
-			throw new Exception("Equipamento sem unidades disponíveis ou não consumível");
+			throw new Exception(this.exceptionEquipmentNoLongerAvailableOrCannotBeUsed);
 		}
 	}
-	
+
 	public boolean canUse() {
 		return quantity > 0 && consumable;
 	}
-	
-	public void addUnits(int qtt) {
+
+	public void addUnits(int qtt) throws Exception {
+		this.checkQuantity(qtt);
+		if (this.quantity + qtt > this.max) {
+			throw new Exception(this.exceptionEquipmentQuantityExceedsMaximum);
+		}
 		quantity += qtt;
 	}
 
+	private void checkQuantity(int quantity) throws Exception {
+		if (quantity < 0) {
+			throw new Exception(this.exceptionEquipmentQuantityMustBePositive);
+		}
+		if (quantity > this.max) {
+			throw new Exception(this.exceptionEquipmentQuantityExceedsMaximum);
+		}
+	}
 }
