0c93e835f698fdb324447b5daadc26412d976b54
beromes <nunesbernardo2000@gmail.com>
2023-06-17 15:39:43 -0300

    refact: utilizando interface para em views existentes

--- src/testedesoftware/clinicacovid/view/SelectModeView.java
+++ src/testedesoftware/clinicacovid/view/SelectModeView.java
@@ -2,28 +2,7 @@ package testedesoftware.clinicacovid.view;
 
 import testedesoftware.clinicacovid.enums.UserRole;
 
-public class SelectModeView extends View {
+public interface SelectModeView {
 
-	@Override
-	public UserRole render() {
-		while(true) {
-			print("Voce é um?");				
-				
-			UserRole[] roleList = UserRole.values();
-			
-			for (Integer i = 0; i < roleList.length; i++) {
-				print((i + 1 ) + ": " + roleList[i].getDescription());
-			}
-			
-			print("Digite a opção: ");
-			
-			int option = in.nextInt();
-			
-			if (option >= 1 && option <= roleList.length) {
-				return roleList[option - 1];
-			}
-			
-			print("Opção inválida");
-		}		
-	}
+	UserRole getUserRole();
 }
