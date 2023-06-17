6ef486e640e60e2978c36b5f7e87d21eaebeb6b2
beromes <nunesbernardo2000@gmail.com>
2023-06-17 11:56:47 -0300

    feat: migrando classes Storage e Equipment para entidades do hibernate

--- src/testedesoftware/clinicacovid/model/Equipment.java
+++ src/testedesoftware/clinicacovid/model/Equipment.java
@@ -1,34 +1,61 @@
 package testedesoftware.clinicacovid.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Equipment {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 
 	private String name;
-	private int quantity;
-	private boolean consumable;
-	private int max;
+	private Integer quantity;
+	private Boolean consumable;
+	private Integer max;
+	
+	@ManyToOne
+    @JoinColumn(name="idStorage", nullable=false)
+	private Storage storage;
 
-	public String exceptionEquipmentNoLongerAvailableOrCannotBeUsed = "Equipamento sem unidades disponíveis ou não consumível";
-	public String exceptionEquipmentQuantityMustBePositive = "Quantidade de um equipamento deve ser positiva.";
-	public String exceptionEquipmentQuantityExceedsMaximum = "A capacidade máxima de armazenamento é " + this.max + ".";
+	public static final String exceptionEquipmentNoLongerAvailableOrCannotBeUsed = "Equipamento sem unidades disponíveis ou não consumível";
+	public static final String exceptionEquipmentQuantityMustBePositive = "Quantidade de um equipamento deve ser positiva.";
+	public static final String exceptionEquipmentQuantityExceedsMaximum = "A capacidade máxima de armazenamento é foi execedida.";
 
-	public Equipment(String name, int quantity, boolean consumable) {
-		super();
+	public Equipment(String name, Integer quantity, Boolean consumable) {
 		this.name = name;
 		this.quantity = quantity;
 		this.consumable = consumable;
 		this.max = 100;
+	}	
+
+	public Integer getId() {
+		return id;
 	}
 
 	public String getName() {
 		return name;
 	}
 
-	public int getQuantity() {
+	public Integer getQuantity() {
 		return quantity;
 	}
 
-	public int getMax() {
+	public Integer getMax() {
 		return max;
+	}
+	
+	public Storage getStorage() {
+		return storage;
+	}
+
+	public void setStorage(Storage storage) {
+		this.storage = storage;
 	}
 
 	public void setQuantity(int quantity) throws Exception {
@@ -45,28 +72,29 @@ public class Equipment {
 			quantity--;
 		}
 		else{
-			throw new Exception(this.exceptionEquipmentNoLongerAvailableOrCannotBeUsed);
+			throw new Exception(exceptionEquipmentNoLongerAvailableOrCannotBeUsed);
 		}
 	}
 
-	public boolean canUse() {
+	public Boolean canUse() {
 		return quantity > 0 && consumable;
 	}
 
 	public void addUnits(int qtt) throws Exception {
 		this.checkQuantity(qtt);
 		if (this.quantity + qtt > this.max) {
-			throw new Exception(this.exceptionEquipmentQuantityExceedsMaximum);
+			throw new Exception(exceptionEquipmentQuantityExceedsMaximum);
 		}
 		quantity += qtt;
 	}
 
 	private void checkQuantity(int quantity) throws Exception {
 		if (quantity < 0) {
-			throw new Exception(this.exceptionEquipmentQuantityMustBePositive);
+			throw new Exception(exceptionEquipmentQuantityMustBePositive);
 		}
 		if (quantity > this.max) {
-			throw new Exception(this.exceptionEquipmentQuantityExceedsMaximum);
+			throw new Exception(exceptionEquipmentQuantityExceedsMaximum);
 		}
 	}
+
 }
