26094e98aa2cecfb282a8d19ee9c1830b6dbc449
beromes <nunesbernardo2000@gmail.com>
2023-06-18 00:53:15 -0300

    fix: corrigindo bug quando usuário não é encontrado

--- src/testedesoftware/clinicacovid/dao/UserDao.java
+++ src/testedesoftware/clinicacovid/dao/UserDao.java
@@ -1,6 +1,7 @@
 package testedesoftware.clinicacovid.dao;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
 import jakarta.persistence.Query;
 import testedesoftware.clinicacovid.dao.UserDao;
 import testedesoftware.clinicacovid.model.User;
@@ -8,16 +9,21 @@ import testedesoftware.clinicacovid.model.User;
 public class UserDao extends GenericDao<User> {
 	
 	public User getByUsername(String username) {
-        EntityManager em = getFactory().createEntityManager();
-        em.getTransaction().begin();
-        
-        Query query = em.createQuery("select u from User u where u.username = :username");
-        query.setParameter("username", username);
-        
-        User user = (User) query.getSingleResult();
-        
-        em.close();
-        
-        return user;
+		try {
+	        EntityManager em = getFactory().createEntityManager();
+	        em.getTransaction().begin();
+	        
+	        Query query = em.createQuery("select u from User u where u.username = :username");
+	        query.setParameter("username", username);
+	        
+	        User user = (User) query.getSingleResult();
+	        
+	        em.close();
+	        
+	        return user;
+	        
+		} catch(NoResultException e) {
+			return null;
+		}
     }
 }
