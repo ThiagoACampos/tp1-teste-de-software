aac98f763366f150823240f2d8617c7b5a032df1
beromes <nunesbernardo2000@gmail.com>
2023-06-18 21:45:27 -0300

    fix: desfazendo alterações no banco de dados quando ocorre alguma exceção

--- src/testedesoftware/clinicacovid/dao/GenericDao.java
+++ src/testedesoftware/clinicacovid/dao/GenericDao.java
@@ -26,33 +26,56 @@ public class GenericDao<T> {
 
     public void saveOrUpdate(T entity) {
     	EntityManager em = getFactory().createEntityManager();
-    	em.getTransaction().begin();    	
-    	em.persist(entity);
-    	em.flush();                
-        em.close();
+    	em.getTransaction().begin();
+    	try {
+    		em.persist(entity);
+        	em.flush();
+    	} catch (Exception e) {
+    		em.getTransaction().rollback();
+    		throw e;
+    	} finally {
+            em.close();	
+    	}
     }
 
     public void delete(T entity) {
     	EntityManager em = getFactory().createEntityManager();
-    	em.getTransaction().begin();    	
-    	em.remove(entity);
-    	em.flush();                
-        em.close();
+    	em.getTransaction().begin();
+    	try {
+    		em.remove(entity);
+        	em.flush();
+    	} catch(Exception e) {
+        	em.getTransaction().rollback();
+    		throw e;
+    	} finally {
+            em.close();
+    	}
     }
 
     public T getById(Integer id) {
     	EntityManager em = getFactory().createEntityManager();
-    	T object = em.find(typeParameterClass, id);
-    	em.close();
-    	return object;
+    	try {
+    		T object = em.find(typeParameterClass, id);
+    		return object;
+    	} catch (Exception e) {
+        	em.getTransaction().rollback();
+        	throw e;
+    	} finally {
+    		em.close();
+    	}
     }
     
     public List<T> findAll() {
-    	EntityManager em = getFactory().createEntityManager();    	
-    	Query q = em.createQuery("select t from " + typeParameterClass.getSimpleName() + " t");
-        List<T> result = (List<T>) q.getResultList();    	
-    	em.close();
-    	return result;
+    	EntityManager em = getFactory().createEntityManager();
+    	try {
+        	Query q = em.createQuery("select t from " + typeParameterClass.getSimpleName() + " t");
+            List<T> result = (List<T>) q.getResultList();
+        	return result;
+    	} catch (Exception e) {
+    		throw e;
+    	} finally {
+        	em.close();
+    	}
     }
 
 }
